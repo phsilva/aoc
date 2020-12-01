a7ea5d7698a1dfec5239bcad920c1de8a8ccd2e0
Paulo Henrique Silva <ph.silva@carta.com>
2020-12-01 13:57:59 -0300

    day1: part 1 solved, non rusty at all

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::File::open("src/day1.input.txt").unwrap();
+    let reader = io::BufReader::new(file);
+    let nums: Vec<i32> = reader
+        .lines()
+        .map(|line| i32::from_str(line.unwrap().as_str()).unwrap())
+        .collect();
+
+    for i in 0..(nums.len() - 1) {
+        let n = nums[i];
+        for j in 1..nums.len() {
+            let m = nums[j];
+            if n + m == 2020 {
+                println!("{} {} {}", n, m, n*m);
+            }
+        }
+    }
 }
