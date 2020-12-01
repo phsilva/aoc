2e6d66e54d9cf2bf62e82b3d0374778308ed33de
Paulo Henrique Silva <ph.silva@carta.com>
2020-12-01 14:08:17 -0300

    day1: part 2 solved, non rusty at all

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{self, BufRead};
 use std::str::FromStr;
 
-fn main() {
+pub fn day1_part_1() {
     let file = fs::File::open("src/day1.input.txt").unwrap();
     let reader = io::BufReader::new(file);
     let nums: Vec<i32> = reader
@@ -12,11 +12,40 @@ fn main() {
 
     for i in 0..(nums.len() - 1) {
         let n = nums[i];
-        for j in 1..nums.len() {
+        for j in (i + 1)..nums.len() {
             let m = nums[j];
             if n + m == 2020 {
-                println!("{} {} {}", n, m, n*m);
+                println!("{} {} {}", n, m, n * m);
+                break;
             }
         }
     }
+}
+
+pub fn day1_part_2() {
+    let file = fs::File::open("src/day1.input.txt").unwrap();
+    let reader = io::BufReader::new(file);
+    let nums: Vec<i32> = reader
+        .lines()
+        .map(|line| i32::from_str(line.unwrap().as_str()).unwrap())
+        .collect();
+
+    for i in 0..(nums.len() - 1) {
+        let n = nums[i];
+        for j in (i + 1)..nums.len() {
+            let m = nums[j];
+            for k in (j + 1)..nums.len() {
+                let o = nums[k];
+                if n + m + o == 2020 {
+                    println!("{} {} {} {}", n, m, o, n * m * o);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    day1_part_1();
+    day1_part_2();
 }
