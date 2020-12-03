ab6995b38364487f472d3619e1df15f8c922ec17
Paulo Henrique Silva <ph.silva@carta.com>
2020-12-02 21:03:01 -0300

    day2: part 1

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,62 @@ pub fn day1_part_2() {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+struct PasswordPolicy {
+    min_chars: i32,
+    max_chars: i32,
+    required_char: char,
+}
+
+impl PasswordPolicy {
+    fn is_valid(self, password: &str) -> bool {
+        let required_chars = password.chars().fold(0, |found, c| {
+            if c == self.required_char {
+                return found + 1;
+            }
+            found
+        });
+        required_chars >= self.min_chars && required_chars <= self.max_chars
+    }
+}
+
+pub fn day2_part_1() {
+    let file = fs::File::open("src/day2.input.txt").unwrap();
+    let reader = io::BufReader::new(file);
+    let passwords: Vec<(String, PasswordPolicy)> = reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let parts: Vec<&str> = line.split(' ').collect();
+            let minmax: Vec<&str> = parts[0].split('-').collect();
+            let min_chars = i32::from_str(minmax[0]).unwrap();
+            let max_chars = i32::from_str(minmax[1]).unwrap();
+            let required_char = parts[1].chars().next().unwrap();
+            let password = parts[2];
+            (
+                String::from(password),
+                PasswordPolicy {
+                    min_chars,
+                    max_chars,
+                    required_char,
+                },
+            )
+        })
+        .collect();
+
+    let mut valid_password = 0;
+    for (password, policy) in passwords {
+        if policy.is_valid(&password) {
+            valid_password += 1
+        }
+    }
+
+    println!("valid password: {}", valid_password);
+}
+
 fn main() {
-    day1_part_1();
-    day1_part_2();
+    // day1_part_1();
+    // day1_part_2();
+    // day2_part_1();
+    day2_part_2();
 }
