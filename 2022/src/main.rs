75aca672126bd698f6352d00de0c2f893d9969ba
Paulo Henrique Silva <ph.silva@gmail.com>
2022-12-01 22:09:29 -0300

    2022: day 1 refactored

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,29 +1,7 @@
 use std::fs;
 use std::io::{self, BufRead};
 
-pub fn day1_part_1() -> i32 {
-    let file = fs::File::open("src/day1.input.txt").unwrap();
-    let reader = io::BufReader::new(file);
-    let calories = reader.lines().map(|line| line.unwrap());
-
-    let mut max_calories: i32 = i32::MIN;
-    let mut current = 0;
-
-    for calorie in calories {
-        if calorie.is_empty() {
-            if current > max_calories {
-                max_calories = current;
-            }
-            current = 0;
-            continue;
-        }
-
-        current += calorie.parse::<i32>().unwrap();
-    }
-    max_calories
-}
-
-pub fn day1_part_2() -> i32 {
+pub fn day1() -> (i32, i32) {
     let file = fs::File::open("src/day1.input.txt").unwrap();
     let reader = io::BufReader::new(file);
     let calories = reader.lines().map(|line| line.unwrap());
@@ -42,7 +20,11 @@ pub fn day1_part_2() -> i32 {
     }
 
     elves.sort();
-    elves.iter().rev().take(3).sum()
+
+    (
+        *elves.iter().max().unwrap(),
+        elves.iter().rev().take(3).sum(),
+    )
 }
 
 fn main() {}
@@ -52,8 +34,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn day1() {
-        assert_eq!(70369, day1_part_1());
-        assert_eq!(203002, day1_part_2());
+    fn test_day1() {
+        assert_eq!((70369, 203002), day1());
     }
 }
