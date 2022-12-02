15dc9261e90e5080e0824db76214ea7f80341964
Paulo Henrique Silva <ph.silva@gmail.com>
2022-12-01 21:56:58 -0300

    2022: day 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,57 +1,51 @@
-use std::io::{self, BufRead};
 use std::fs;
+use std::io::{self, BufRead};
 
 pub fn day1_part_1() -> i32 {
-    let file = fs::File::open("2020/src/day1.input.txt").unwrap();
+    let file = fs::File::open("src/day1.input.txt").unwrap();
     let reader = io::BufReader::new(file);
-    let nums: Vec<i32> = reader
-        .lines()
-        .map(|line| line.unwrap().parse().unwrap())
-        .collect();
+    let calories = reader.lines().map(|line| line.unwrap());
 
-    let mut answer: i32 = 0;
+    let mut max_calories: i32 = i32::MIN;
+    let mut current = 0;
 
-    for (i, n) in nums.iter().enumerate() {
-        for m in nums.iter().skip(i + 1) {
-            if n + m == 2020 {
-                println!("day1/1: {}", n * m);
-                answer = n * m;
+    for calorie in calories {
+        if calorie.is_empty() {
+            if current > max_calories {
+                max_calories = current;
             }
+            current = 0;
+            continue;
         }
-    }
 
-    answer
+        current += calorie.parse::<i32>().unwrap();
+    }
+    max_calories
 }
 
 pub fn day1_part_2() -> i32 {
-    let file = fs::File::open("2020/src/day1.input.txt").unwrap();
+    let file = fs::File::open("src/day1.input.txt").unwrap();
     let reader = io::BufReader::new(file);
-    let nums: Vec<i32> = reader
-        .lines()
-        .map(|line| line.unwrap().parse().unwrap())
-        .collect();
+    let calories = reader.lines().map(|line| line.unwrap());
 
-    let mut answer: i32 = 0;
+    let mut current = 0;
+    let mut elves = Vec::new();
 
-    for (i, n) in nums.iter().enumerate() {
-        for (j, m) in nums.iter().enumerate().skip(i + 1) {
-            for o in nums.iter().skip(j + 1) {
-                if n + m + o == 2020 {
-                    answer = n * m * o;
-                    println!("day1/2: {}", answer);
-                    break;
-                }
-            }
+    for calorie in calories {
+        if calorie.is_empty() {
+            elves.push(current);
+            current = 0;
+            continue;
         }
+
+        current += calorie.parse::<i32>().unwrap();
     }
 
-    answer
+    elves.sort();
+    elves.iter().rev().take(3).sum()
 }
 
-fn main() {
-    day1_part_1();
-    day1_part_2();
-}
+fn main() {}
 
 #[cfg(test)]
 mod test {
@@ -59,8 +53,7 @@ mod test {
 
     #[test]
     fn day1() {
-        assert_eq!(712075, day1_part_1());
-        assert_eq!(145245270, day1_part_2());
+        assert_eq!(70369, day1_part_1());
+        assert_eq!(203002, day1_part_2());
     }
-
 }
